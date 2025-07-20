fbb652529d323ee2c870dac8c0bcb8b229fca7b3
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-07-20 20:26:27 +0200

    ♻️ update : Refactor store setup to use Redux Toolkit
    Replaced legacy Redux store creation with configureStore from @reduxjs/toolkit. Integrated counter and auth reducers for improved modularity and maintainability.

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,42 @@
-import {createStore, redux} from "redux";
+import {configureStore} from "@reduxjs/toolkit";
+import counterReducer from "./counter-store";
+import authReducer from "./auth-store";
 
-const counterReducer = (state = {counter:0}, action) => {
-    if (action.type === "INCREMENT") {
-        return{
-            counter : state.counter + 1,
-        }
-    }
-    if (action.type === "DECREMENT") {
-        return {
-            counter : state.counter - 1,
-        }
-    }
-    return state;
-}
-const store = createStore(counterReducer);
+
+// Old Way to create Reducer
+// const counterReducer = (state = initialState, action) => {
+//     if (action.type === "INCREMENT") {
+//         return{
+//             counter : state.counter + 1,
+//             showCounter: state.showCounter
+//         }
+//     }
+//     if(action.type === "INCREMENT_BY_5") {
+//         return {
+//             counter : state.counter + action.amount,
+//             showCounter: state.showCounter
+//         }
+//     }
+//     if (action.type === "DECREMENT") {
+//         return {
+//             counter : state.counter - 1,
+//             showCounter: state.showCounter
+//         }
+//     }
+//     if(action.type === "TOGGLE_COUNTER"){
+//         return {
+//             counter : state.counter,
+//             showCounter: !state.showCounter
+//         }
+//     }
+//     return state;
+// }
+const store = configureStore({
+    reducer: {
+        counter: counterReducer,
+        auth: authReducer,
+
+    }});
+
+
 export default store;

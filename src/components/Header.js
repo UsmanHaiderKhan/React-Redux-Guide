772e7b6fbc496ebb5a5ddacef229cc86f17e310d
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-07-20 20:26:50 +0200

    ♻️ update : Add authentication logic to App and Header
    Integrated Redux authentication state into App and Header components. App now conditionally renders Auth and UserProfile based on authentication status. Header displays Login/Logout buttons and handles logout via Redux.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,13 @@
 import classes from './Header.module.css';
+import {useDispatch, useSelector} from "react-redux";
+import {authActions} from "../store/auth-store";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const isAuth = useSelector(state => state.auth.isAuthenticated);
+  const logoutHandler = () =>{
+    dispatch(authActions.logout());
+  }
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
@@ -13,7 +20,8 @@ const Header = () => {
             <a href='/'>My Sales</a>
           </li>
           <li>
-            <button>Logout</button>
+            {isAuth && <button onClick={logoutHandler}>Logout</button>}
+            {!isAuth && <button>Login</button>}
           </li>
         </ul>
       </nav>

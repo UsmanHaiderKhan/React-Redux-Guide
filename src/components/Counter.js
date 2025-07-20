e683f85c6a12026310859c470d8747f3f645c9f8
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-07-20 20:25:54 +0200

    ♻️ Update : Refactor Auth and Counter components to use Redux actions
    Updated Auth.js to dispatch login action using Redux and handle form submission. Refactored Counter.js to use Redux actions for increment, decrement, increment by 5, and toggle counter visibility, removing unused imports and improving state selection.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,24 +1,32 @@
-import {Component} from 'react';
-import {useSelector, useDispatch, connect} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+import {counterActions} from "../store/counter-store";
 import classes from './Counter.module.css';
+
 
 const Counter = () => {
     const dispatch = useDispatch();
-   const counter= useSelector((state) => state.counter);
+   const counter= useSelector((state) => state.counter.counter);
+   const showCounter = useSelector((state) => state.counter.showCounter);
    const incrementHandler = ()=>{
-       dispatch({ type: 'INCREMENT' });
+       dispatch(counterActions.increment());
    };
+    const incrementBy5Handler = () =>{
+        dispatch(counterActions.incrementBy5(5));
+    }
    const decrementHandler = ()=>{
-       dispatch({ type: 'DECREMENT' });
+       dispatch(counterActions.decrement());
    };
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+      dispatch(counterActions.toggleCounter());
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+        { !showCounter  && <div className={classes.value}>{counter}</div>}
         <div>
             <button onClick={incrementHandler}>Increment</button>
+            <button onClick={incrementBy5Handler}>Increment By 5</button>
             <button onClick={decrementHandler}>Decrement</button>
         </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
